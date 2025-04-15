f9bf5d66b6cbb2e7614420c12b5c41159f519d59
martin <maa.velazquez@alumno.um.edu.ar>
2025-04-15 13:26:59 -0300

    implementar la comparación de los caracteres que se introducen

--- src/is_palindrome.py
+++ src/is_palindrome.py
@@ -13,3 +13,8 @@ if __name__ == "__main__":
         while True:
             entrada = input("Ingrese una palabra o frase: ")
             if is_palindrome(entrada):
+                print(f'"{entrada}" es un palíndromo\n')
+            else:
+                print(f'"{entrada}" no es un palíndromo\n')
+    except KeyboardInterrupt:
+        print("\nPrograma finalizado.")
